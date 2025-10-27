2a2ed7fffb3a1dc629cf1eb35b378f367b2ec62b
Peyton Howe <peyton.howe@bellsouth.net>
2025-10-26 22:35:43 -0700

    Add initial support for Questing

---
+++ config/suites/questing.sh
@@ -0,0 +1,10 @@
+# shellcheck shell=bash
+
+export RELASE_NAME="Ubuntu 25.10 (Questing Quokka)"
+export RELASE_VERSION="25.10"
+
+export KERNEL_REPO="https://github.com/peyton-howe/linux-rockchip.git"
+export KERNEL_BRANCH="camera_drivers"
+export KERNEL_FLAVOR="rockchip"
+
+export EXTRA_PPAS="phowe6/rockchip"
